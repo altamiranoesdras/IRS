a341a49f155811cf009b34ac363f3093f303f063
Esdras Altamirano <altamiranoesdras@gmail.com>
2018-11-04 21:40:25 -0600

    Reescrito metodo showLogin form y editada la visa auth.login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -36,4 +36,11 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
     }
+
+    public function showLoginForm()
+    {
+        return view('auth.login');
+    }
+
+
 }
