14f40d1976d3476f40fb779ffa8c38f769c3e20f
Esdras Altamirano <altamiranoesdras@gmail.com>
2018-11-10 20:57:16 -0600

    Plantilla para plugin bootstrap-fileinput

---
+++ resources/views/layouts/plugins/bootstrap_fileinput.blade.php
@@ -0,0 +1,7 @@
+@push('css')
+<link rel="stylesheet" href="{{asset('css/fileinput.min.css')}}" />
+@endpush
+@push('js')
+<script type="text/javascript" src="{{asset('js/fileinput.min.js')}}"></script>
+<script type="text/javascript" src="{{asset('js/fileinput_plugins/es.js')}}"></script>
+@endpush
